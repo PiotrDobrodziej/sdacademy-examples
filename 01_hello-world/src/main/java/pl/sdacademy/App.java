47c0bee9452283b8d4a7c62ad804b396984046c6
Piotr Dobrodziej <pidobr@wp.pl>
2018-12-08 14:51:01 +0100

    add remark from Piotr

--- 01_hello-world/src/main/java/pl/sdacademy/App.java
+++ 01_hello-world/src/main/java/pl/sdacademy/App.java
@@ -2,9 +2,11 @@ package pl.sdacademy;
 
 /**
  * Hello world!
+ * Hello from Piotr
  */
 public class App {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 	}
+
 }
